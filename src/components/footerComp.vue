edcebb6040fc1209e3de1f7355b5821e4a9b33a5
FraFra2 <kekkino41@gmail.com>
2023-07-26 14:10:04 +0200

    ADD Footer 50%

--- src/components/footerComp.vue
+++ src/components/footerComp.vue
@@ -1,19 +1,78 @@
 <script>
+import dotbarComp from './dotbarComp.vue';
 export default {
   data() {
     return {
-      count: 0
+      quickLinks :[
+        {
+          label: "Careers",
+          ref: "#"
+        },
+        {
+          label: "News",
+          ref: "#"
+        },
+        {
+          label: "Terms of use",
+          ref: "#"
+        },
+        {
+          label: "Privacy Projects",
+          ref: "#"
+        },
+        {
+          label: "About",
+          ref: "#"
+        },
+        {
+          label: "Contact",
+          ref: "#"
+        },
+      ] 
     }
+  },
+  components:{
+    dotbarComp
   }
 }
 </script>
 
 <template>
-    <footer>
-      Footer
+    <footer class = "mt-32">
+      <div class = "grid grid-cols-4 px-16">
+        <div>
+          <img src="src/assets/cropped-Group-39-2x.png">
+          <p class = "text-sm py-8">
+            While the lovely valley teems with vapour around me, and the meridian sun strikes
+            the upper surfaces of the imprenetrable foliage of my trees. While the lovely
+            valleys teems with vapour around me.
+          </p>
+        </div>
+        <div>
+          <h2>
+            QUICK LINKS
+          </h2>
+          <dotbarComp/>
+        </div>
+        <div>
+          <h2>
+            CONTACT
+          </h2>
+          <dotbarComp/>
+        </div>
+        <div>
+          <h2>
+            LOCATION ON MAP
+          </h2>
+          <dotbarComp/>
+          <img class = "py-4" src="src/assets/map.png">
+        </div>
+      </div>
     </footer>
 </template>
 
 <style scoped>
-
+h2{
+  font-weight: bold;
+}
 </style>
