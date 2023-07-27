f130c928ccc89ad0e6381f3106e91a8b01f8cbbd
FraFra2 <kekkino41@gmail.com>
2023-07-27 02:28:24 +0200

    ADD fix 0.2

--- src/components/missionComp.vue
+++ src/components/missionComp.vue
@@ -16,9 +16,26 @@ export default {
 </script>
 
 <template>
-<div class="flex py-40">
-  <div class = "w-1/2">
-    <img class = "rounded-r-3xl" src="src/assets/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt="">
+<div class="flex justify-between py-40">
+  <div class = "w-[40rem] relative">
+
+    <div class = "absolute -my-14 w-full h-full ">
+      <div class = "relative w-5/6 h-5/6">
+
+        <img class = "absolute w-full h-full top-0 left-0 rounded-r-[2rem]" src="src/assets/busy-architect-PYVKWM4-1024x872.jpg">
+        <div class = "overlay opacity-90 absolute w-full h-full top-0 left-0 rounded-r-[2rem]">
+
+        </div>
+      </div>
+    </div>
+
+    <div class = "m-image-top bg-top ml-32 -mr-12 bg-cover h-[40%] drop-shadow-lg rounded-t-lg">
+
+    </div>
+    <div class = "m-image-bottom bg-bottom ml-20 bg-cover h-[40%] drop-shadow-lg rounded-b-lg">
+      
+    </div>
+
   </div>
   <div class = "w-1/2 px-16 py-16">
     
@@ -37,5 +54,17 @@ export default {
 </template>
 
 <style scoped>
+.m-image-bottom{
+  background-image: url('src/assets/businesswoman-analysing-document-P8WSNMC-1024x820.jpg');
+}
+.m-image-top{
+  background-image: url('src/assets/businesswoman-analysing-document-P8WSNMC-1024x820.jpg');
+
+}
+
+.overlay{
+  background: rgb(152,225,94);
+  background: linear-gradient(0deg, rgba(152,225,94,1) 25%, rgba(3,217,164,1) 75%);
+}
 
 </style>
--- src/components/projectsComp.vue
+++ src/components/projectsComp.vue
@@ -7,38 +7,37 @@ export default {
     return {
       images: [
           {
-            title: "Ina-soulis",
-            img: "src/assets/ina-soulis-227104-unsplash-1024x1024.jpg"
+            title: "A Basket Of Flower",
+            img: "src/assets/ina-soulis-227104-unsplash-1024x1024.jpg",
           },
           {
-            title: "Ina-soulis",
+            title: "A Famous Ferris Wheel",
             img: "src/assets/sunisa-misa-531163-unsplash-1024x1024.jpg"
           },
           {
-            title: "Ina-soulis",
+            title: "A Pair Of Yellow Shoes",
             img: "src/assets/355H-1024x1024.jpg"
           },
           {
-            title: "Ina-soulis",
+            title: "A Notebook",
             img: "src/assets/photo-1448932252197-d19750584e56-1024x1024.jpg"
           },
           {
-            title: "Ina-soulis",
+            title: "Paper Boats",
             img: "src/assets/business-competition-PB366D8-1024x1024.jpg"
           },
           {
-            title: "Ina-soulis",
+            title: "A Cozy Sofa",
             img: "src/assets/cozy-sofa-in-living-room-PQR5AB9-1024x1024.jpg"
           },
           {
-            title: "Ina-soulis",
+            title: "Hot Baloons",
             img: "src/assets/aa9a4539-PQGJ7HU-1024x1024.jpg"
           },
           {
-            title: "Ina-soulis",
+            title: "A Blue Orange",
             img: "src/assets/cody-davis-253928-unsplash-1024x1024.jpg"
           },
-          
       ]
     }
   },
@@ -71,13 +70,53 @@ export default {
     </div>
   </div>
 
-  <div class = "grid grid-cols-4 grid-rows-2 px-2 gap-2">
-    <div v-for = "img in images" class = "group">
+  <div class = "grid grid-cols-4 grid-rows-2 px-8 gap-3.5">
+    <div v-for = "img in images" class = "
+    group relative 
+    ">
       <img class = "
-      rounded-sm 
-      group-hover:scale-110" :src="img.img" :alt="img.title">
+      rounded-md
+      z-10
+      group-hover:scale-110 transition-all" :src="img.img" :alt="img.title">
+      <div class = "
+      m-filter hidden
+      rounded-md
+      group-hover:inline-block group-hover:scale-110
+      w-full h-full
+      opacity-50
+      z-10
+      top-0
+      absolute
+      
+      ">
+      </div>
+      <div class = "hidden
+      rounded-md
+      group-hover:inline-block group-hover:scale-110
+      w-full h-full
+      opacity-100
+      z-20
+      top-0
+      absolute">
+        <div class = "
+        flex flex-col-reverse justify-start items-center
+        h-full text-white font-bold
+        ">
+          <h3 class ="font-bold py-8">
+            Marketing
+          </h3>
+          <h2 class = "text-2xl">
+            {{ img.title }}
+          </h2>
+        </div>
+      </div>
     </div>
+
   </div>
+
+
+
+
 
   <div class = "flex justify-center items-center pt-24 pb-32">
     <buttonComp green shadow content = "READ MORE"/>
@@ -85,4 +124,8 @@ export default {
 </template>
 
 <style scoped>
+.m-filter{
+  background: rgb(152,225,94);
+  background: linear-gradient(90deg, rgba(152,225,94,1) 25%, rgba(3,217,164,1) 75%);
+}
 </style>
